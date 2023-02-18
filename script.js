4797cc54e475176128070c6f149eab50736bec89
karishma gupta <karishma15102002@gmail.com>
2023-02-18 23:14:15 +0530

    added

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 let arr = [];  // empty array as of now
-let n = 0;   //no.of books issued is 0
-console.log('hello');
 
-function issueBook(){
+function issueBook() {
    console.log("button");
    var bookName = document.getElementById("book").value;
    var issuedTo = document.getElementById("issued-book-to").value;
+   var id = arr.length+1;
    let time = new Date();
+   let status= "not returned";
 
-   let bookObj = {id: n+1, book_name:bookName, issued_to:issuedTo, time: time , status:"not returned"}
+   let bookObj = {id: id, book_name:bookName, issued_to:issuedTo, time: time , status:status}
    arr.push(bookObj);
-   n++;
+
+   addtable();
 }
 
-function print(){
-   console.log("helo");
+function addtable(){
+   let tableBody = document.querySelector('.table-body');
+
+   tableBody.innerHTML = "";
+
+   let row = document.createElement('tr');
 }
 
-console.log(arr);
+
